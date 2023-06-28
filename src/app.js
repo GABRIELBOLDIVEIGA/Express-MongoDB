3c8f75b6a64604e9bb8b7b6bdfc82dd7e71650a1
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-06-28 19:16:46 -0300

    ...

--- src/app.js
+++ src/app.js
@@ -1,33 +1,14 @@
-import express from 'express';
+import express from "express";
+import db from "./config/dbConnect.js";
+import routes from "./routes/index.js";
+
+db.on("error", console.log.bind(console, "Erro de conexão"));
+db.once("open", () => {
+  console.log("conexão com o banco feita com sucesso");
+});
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+routes(app);
 
-const livros = [
-  { id: 1, titulo: "Senhor dos aneis" },
-  { id: 2, titulo: "Dunna" }
-]
-
-app.get('/', (req, res) => {
-  res.status(200).send("Curso de Node")
-})
-
-app.get('/livros', (req, res) => {
-  res.status(200).json(livros)
-})
-
-app.post('/livros', (req, res) => {
-  const livro = req.body
-  livros.push(livro)
-  res.status(201).json(livro)
-})
-
-app.put('/livros/:id', (req, res) => {
-  const id = req.params
-  const livro = req.body
-
-  livros.push(livro)
-  res.status(201).json(livro)
-})
-
-export default app
+export default app;
